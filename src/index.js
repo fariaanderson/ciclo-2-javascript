3a7bd9ab5b902ed3b561c96afe9215a6cd389073
Andrea <dea0312@gmail.com>
2024-03-12 20:24:00 -0300

    Deixando string em caixa alta e verificando quantidade de letras.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ var empty = "";
 console.log(
   "Anderson possui " + x.length + " unidades de código de comprimento",
 );
+
+
+let nome =  "Andrea";
+
+console.log(`A string ${nome.toUpperCase()} possui ${nome.length} letras.`);
